83999560ae1ef82b18874bbfef2ce0dc31d55c3d
ebe <130290570+akaebe@users.noreply.github.com>
2024-06-01 06:55:00 +0000

    my portfolio resume

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "EBENEZER",
+  title: "Web Developer and Software Engineer",
+  email: "ebelivingstone0@gmail.com",
+  gitHub: "github.com/akaebe",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  linkedIn: "linkedin.com/in/ebenezer-livingstone",
 };
 
 const primaryColor = "#4E567E";
